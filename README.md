09c3b58d2e5fa307097d0883a03f5c5394af1d1f
AgungDev <ganzide404@gmail.com>
2025-12-19 18:44:54 +0900

    [UPDATE] Simple Crud

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # WEBGIS
 
-run
+Aplikasi Web GIS ini dikembangkan untuk menampilkan informasi spasial Provinsi Maluku Utara menggunakan Laravel sebagai backend dan Leaflet sebagai frontend. Data spasial diolah menggunakan QGIS dan disajikan dalam format GeoJSON sehingga dapat ditampilkan secara interaktif melalui web browser.
 
+```sh
 docker compose up --build
+```
+
+### Database
+
+Jika ingin cek database, di sini.
+
+```sh
+docker compose exec postgres psql -U webgis -d webgi
+\dt
+select * from location;
+```
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LocationController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', fn() => view('map'));
+
+Route::post('/locations', [LocationController::class, 'store']);
+Route::get('/locations', [LocationController::class, 'index']);
+Route::delete('/locations/{id}', function ($id) {
+    \App\Models\Location::find($id)->delete();
+    return response()->json(['ok']);
 });
+
