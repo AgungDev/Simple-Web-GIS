bde09db167f27b181ec4d97277f68c67ca12f4f4
AgungDev <ganzide404@gmail.com>
2025-12-20 19:17:31 +0900

    [FIX] Simple Webgis Maluku Utara

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # WEBGIS
 
-Aplikasi Web GIS ini dikembangkan untuk menampilkan informasi spasial Provinsi Maluku Utara menggunakan Laravel sebagai backend dan Leaflet sebagai frontend. Data spasial diolah menggunakan QGIS dan disajikan dalam format GeoJSON sehingga dapat ditampilkan secara interaktif melalui web browser.
+Proyek ini bertujuan untuk membangun sebuah Web GIS sederhana yang menampilkan dan mengelola data spasial wilayah Provinsi Maluku Utara. Sistem dikembangkan menggunakan Laravel sebagai backend dan Leaflet.js sebagai library pemetaan interaktif.
+
+Web GIS ini mampu menampilkan peta dasar (OpenStreetMap), layer hasil digitasi dan ekspor dari QGIS, serta menyediakan fitur input data lokasi secara langsung melalui peta. Pengguna dapat menambahkan titik lokasi dengan cara mengklik peta, kemudian menyimpan informasi koordinat (latitude dan longitude) beserta atribut pendukung ke dalam basis data PostgreSQL.
+
+Selain itu, aplikasi ini juga mendukung proses pengelolaan data (CRUD), yaitu menampilkan, menambahkan, dan menghapus data lokasi yang tersimpan, baik dalam bentuk tabel maupun marker pada peta. Dengan adanya sistem ini, pengguna dapat memahami konsep integrasi data spasial dan non-spasial serta penerapan Web GIS dalam pengelolaan informasi geografis berbasis web.
+
+```sh
+docker pull ketekperry/simple-webgis-malut:latest
+docker run -p 8000:8000 ketekperry/simple-webgis-malut:latest
+```
+
+atau
 
 ```sh
 docker compose up --build
--- resources/views/map.bck.blade.php
+++ resources/views/map.bck.blade.php
@@ -4,26 +4,10 @@
 <head>
     <title>Input Data Web GIS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <meta name="mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <link rel="stylesheet" href="{{ asset('css/leaflet.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/L.Control.Layers.Tree.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/qgis2web.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/fontawesome-all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/leaflet.photon.css') }}">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     <style>
         #map {
             height: 400px;
-        }
-
-        html,
-        body {
-            height: auto;
-            overflow-y: auto;
         }
     </style>
 
@@ -77,50 +61,13 @@
         </table>
     </div>
 
-    <!-- Scirpt -->
-    <script src="{{ asset('js/qgis2web_expressions.js') }}"></script>
-    <script src="{{ asset('js/leaflet.js') }}"></script>
-    <script src="{{ asset('js/L.Control.Layers.Tree.min.js') }}"></script>
-    <script src="{{ asset('js/leaflet.rotatedMarker.js') }}"></script>
-    <script src="{{ asset('js/leaflet.pattern.js') }}"></script>
-    <script src="{{ asset('js/leaflet-hash.js') }}"></script>
-    <script src="{{ asset('js/Autolinker.min.js') }}"></script>
-    <script src="{{ asset('js/rbush.min.js') }}"></script>
-    <script src="{{ asset('js/labelgun.min.js') }}"></script>
-    <script src="{{ asset('js/labels.js') }}"></script>
-    <script src="{{ asset('js/leaflet.photon.js') }}"></script>
-    <script src="{{ asset('data/GeologyMalukuUtara_1.js') }}"></script>
-    <script src="{{ asset('data/ADMINISTRASIKECAMATAN_AR_50K_2.js') }}"></script>
-    <script src="{{ asset('data/AGRIKEBUN_AR_50K_3.js') }}"></script>
-    <script src="{{ asset('data/AIRPORT_AR_50K_4.js') }}"></script>
-    <script src="{{ asset('data/AIRPORT_PT_50K_5.js') }}"></script>
-    <script src="{{ asset('data/BANGUNAN_AR_50K_6.js') }}"></script>
-    <script src="{{ asset('data/BANGUNAN_PT_50K_7.js') }}"></script>
-    <script src="{{ asset('data/CAGARBUDAYA_PT_50K_8.js') }}"></script>
-    <script src="{{ asset('data/DANAU_AR_50K_9.js') }}"></script>
-    <script src="{{ asset('data/DERMAGA_PT_50K_10.js') }}"></script>
-    <script src="{{ asset('data/GARISRPANTAI_LN_50K_11.js') }}"></script>
-    <script src="{{ asset('data/JALAN_LN_50K_12.js') }}"></script>
-    <script src="{{ asset('data/KESEHATAN_PT_50K_13.js') }}"></script>
-    <script src="{{ asset('data/KONTUR_LN_50K_14.js') }}"></script>
-    <script src="{{ asset('data/MAKAM_PT_50K_15.js') }}"></script>
-    <script src="{{ asset('data/NIAGA_PT_50K_16.js') }}"></script>
-    <script src="{{ asset('data/NONAGRIALANG_AR_50K_17.js') }}"></script>
-    <script src="{{ asset('data/NONAGRIHUTANKERING_AR_50K_18.js') }}"></script>
-    <script src="{{ asset('data/NONAGRISEMAKBELUKAR_AR_50K_19.js') }}"></script>
-    <script src="{{ asset('data/PELABUHAN_PT_50K_20.js') }}"></script>
-    <script src="{{ asset('data/PEMERINTAHAN_PT_50K_21.js') }}"></script>
-    <script src="{{ asset('data/PENDIDIKAN_PT_50K_22.js') }}"></script>
-    <script src="{{ asset('data/PILARBATAS_PT_50K_23.js') }}"></script>
-    <script src="{{ asset('data/PUSKESMAS_PT_50K_24.js') }}"></script>
-    <script src="{{ asset('data/RUMAHSAKIT_PT_50K_25.js') }}"></script>
-    <script src="{{ asset('data/SARANAIBADAH_PT_50K_26.js') }}"></script>
-    <script src="{{ asset('data/SUNGAI_AR_50K_27.js') }}"></script>
-    <script src="{{ asset('data/SUNGAI_LN_50K_28.js') }}"></script>
-    <script src="{{ asset('data/TOPONIMI_PT_50K_29.js') }}"></script>
-    <script src="{{ asset('data/ADMINISTRASI_LN_50K_30.js') }}"></script>
-    <script src="{{ asset('js/malutmap.js') }}"></script>
+
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
     <script>
+        const map = L.map('map').setView([0.8, 127.4], 7);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+            .addTo(map);
 
         let markerTemp = null;
 
